edcfd330064f0f0cf58cff4d7fcbedd35913cbf3
tai2 <hello@tai2.net>
2020-12-18 15:30:48 +0900

    Add dependency resolution.

--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,5 @@
 import { readFileSync as readFile } from 'fs';
+import path from 'path';
 import readData from 'data-uri-to-buffer';
 import mimeTypes from 'mime-types';
 import fetch from 'make-fetch-happen';
@@ -61,11 +62,51 @@ async function loadURL(url, fetchOpts) {
   }
 }
 
+function isBearSpecifier(specifier) {
+  if (path.extname(specifier) !== '') {
+    return false
+  }
+
+  if (specifier.startsWith('@')) {
+    return specifier.split('/').length === 2
+  }
+
+  return specifier.split('/').length === 1
+}
+
+function packageName(absolutePath) {
+  const segments = absolutePath.split('/')
+  if (absolutePath.startsWith('/@')) {
+    return path.join(segments[1], segments[2])
+  }
+
+  return segments[1]
+}
+
 export default function urlResolve(fetchOpts) {
   return {
-    resolveId(source) {
+    async resolveId(source, importer) {
       const url = parseURL(source);
-      return url ? resolveURL(url) : null;
+      if (url) {
+        return resolveURL(url)
+      }
+
+      const importerUrl = parseURL(importer);
+      if (!importerUrl) {
+        return null
+      }
+
+      if (isBearSpecifier(source)) {
+        const importerPackage = packageName(importerUrl.pathname)
+        importerUrl.pathname = path.join(importerPackage, 'package.json')
+        const packageJson = JSON.parse(await loadURL(importerUrl, fetchOpts))
+        const version = packageJson.dependencies[source]
+        importerUrl.pathname = `${source}@${version}`
+        return importerUrl.toString()
+      }
+
+      importerUrl.pathname = source
+      return importerUrl.toString()
     },
     load(id) {
       const url = parseURL(id);
